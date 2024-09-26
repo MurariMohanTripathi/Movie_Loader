abc6cb4328f3ff03b55864b4787c232f920ede34
MURARI <tripathimurari599@gmail.com>
2024-09-26 20:05:35 +0530

    prepare something big comming3

--- src/App.js
+++ src/App.js
@@ -11,10 +11,19 @@ const App = ()=>{
     const [movies,setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const searchMovies = async (title) =>{
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search);
+        try {
+            const response = await fetch(`${API_URL}&s=${title}`);
+            const data = await response.json();
+    
+            if (data.Response === 'True') {
+                setMovies(data.Search);
+            } else {
+                setMovies([]); // Clear the movies state if no movies are found
+            }
+        } catch (error) {
+            console.error("Error fetching the movies:", error);
+            setMovies([]); // Clear the movies state in case of an error
+        }
     }
     useEffect(()=>{
         searchMovies('spiderman');
